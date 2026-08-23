c41510ebb14d88624ce857c8dd2c87710676d522
Sergey Kazakov <sergey.kazakov@vseinstrumenti.ru>
2026-08-23 23:28:39 +0300

    hw06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,38 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	for _, stage := range stages {
+		out = stage(doneSignal(done, out))
+	}
+	return out
+}
+
+func doneSignal(done In, out Out) Out {
+	result := make(Bi)
+
+	go func() {
+		defer func() {
+			for range out { //nolint:revive
+			}
+		}()
+		defer close(result)
+
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-out:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					return
+				case result <- v:
+				}
+			}
+		}
+	}()
+	return result
 }
