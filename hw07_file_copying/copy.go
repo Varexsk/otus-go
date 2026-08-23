e9952d8548968655b877519d11c44c9f67bb0f20
Sergey Kazakov <sergey.kazakov@vseinstrumenti.ru>
2026-08-24 01:04:04 +0300

    hw07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,63 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
+	ErrNotExist              = errors.New("file not exist")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if errors.Is(err, fs.ErrNotExist) {
+		return ErrNotExist
+	} else if err != nil {
+		return fmt.Errorf("open source file: %w", err)
+	}
+	defer sourceFile.Close()
+
+	fileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return fmt.Errorf("source stat: %w", err)
+	}
+	if !fileInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	if offset > fileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	toCopy := fileInfo.Size() - offset
+	if limit > 0 && limit < toCopy {
+		toCopy = limit
+	}
+
+	if _, err = sourceFile.Seek(offset, io.SeekStart); err != nil {
+		return fmt.Errorf("seek: %w", err)
+	}
+
+	destFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("create destination: %w", err)
+	}
+	defer destFile.Close()
+
+	bar := pb.Full.Start64(toCopy)
+	defer bar.Finish()
+	barWriter := bar.NewProxyWriter(destFile)
+
+	_, err = io.CopyN(barWriter, sourceFile, toCopy)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("failed to copy file: %w", err)
+	}
+
 	return nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,14 @@
 module github.com/fixme_my_friend/hw07_file_copying
 
-go 1.23
+go 1.25.0
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/cheggaaa/pb/v3 v3.2.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.2.0 // indirect
+	github.com/fatih/color v1.19.0 // indirect
+	github.com/mattn/go-colorable v0.1.15 // indirect
+	github.com/mattn/go-isatty v0.0.24 // indirect
+	github.com/mattn/go-runewidth v0.0.27 // indirect
+	golang.org/x/sys v0.47.0 // indirect
+)
